4806f25eb39d2257e1e695bb56686eecb28e0ff1
Yash Djson Dookun <dookun88@gmail.com>
2020-05-13 19:59:07 +0400

    Removed one line

--- app/meme.py
+++ app/meme.py
@@ -7,8 +7,7 @@ with open("memes.json", encoding="utf8") as question_json:
     questions_list = json.load(question_json)
     random_question = (str(questions_list["memes"][random.randint(0,26)]).split(r": '")[1]).split("'}")[0]
 
-meme = PIL.Image.open(f"blank_images/{random.randint(1,10)}.png")
-meme = meme.filter(PIL.ImageFilter.GaussianBlur(3))
+meme = PIL.Image.open(f"blank_images/{random.randint(1,10)}.png").filter(PIL.ImageFilter.GaussianBlur(3))
 draw = PIL.ImageDraw.Draw(meme)
 font = PIL.ImageFont.truetype(r"font/OperatorMono-Medium.otf",30)
 
