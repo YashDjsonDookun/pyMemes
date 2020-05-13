63bae8de6b3f73f05ffec50a9b5ecaaeb068c41e
Yash Djson Dookun <dookun88@gmail.com>
2020-05-13 21:45:14 +0400

    added .exe and more memes

--- app/meme.py
+++ app/meme.py
@@ -2,20 +2,45 @@ import json
 import random
 import PIL.Image, PIL.ImageFont, PIL.ImageDraw, PIL.ImageFilter
 import textwrap
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QVBoxLayout
 
-with open("memes.json", encoding="utf8") as question_json:
-    questions_list = json.load(question_json)
-    random_question = (str(questions_list["memes"][random.randint(0,26)]).split(r": '")[1]).split("'}")[0]
+def generate_meme():
+    try:
+        with open("memes.json", encoding="utf8") as question_json:
+            questions_list = json.load(question_json)
+            random_question = (str(questions_list["memes"][random.randint(0,114)]).split(r": '")[1]).split("'}")[0]
 
-meme = PIL.Image.open(f"blank_images/{random.randint(1,10)}.png").filter(PIL.ImageFilter.GaussianBlur(3))
-draw = PIL.ImageDraw.Draw(meme)
-font = PIL.ImageFont.truetype(r"font/OperatorMono-Medium.otf",30)
+        meme = PIL.Image.open(f"blank_images/{random.randint(1,38)}.png").filter(PIL.ImageFilter.MaxFilter(1)).filter(PIL.ImageFilter.GaussianBlur(2.2))
+        draw = PIL.ImageDraw.Draw(meme)
+        font = PIL.ImageFont.truetype(r"font/OperatorMono-Medium.otf",23)
 
-meme_string = ""
-lines = textwrap.wrap(random_question, 25)
-for line in lines:
-    meme_string += (f"{line}\n")
+        meme_string = ""
+        lines = textwrap.wrap(random_question, 30)
+        for line in lines:
+            meme_string += (f"{line}\n")
 
-print(meme_string.strip())
-draw.text((20,20), meme_string, font= font, fill="white", align="center")
-meme.show()
+        # colors = ["white","black"]
+        placements = [(30,170), (50,50), (60,175), (70,20), (100,150)]
+        print(meme_string.strip())
+        # draw.text(placements[random.randint(0,4)], meme_string, font= font, fill=colors[random.randint(0,1)], align="center")
+        draw.text(placements[random.randint(0,4)], meme_string, font= font, fill="white", align="center")
+        meme.show()
+    except Exception as e:
+        generate_meme()
+
+meme_app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle("Bad Meme Generator")
+window.setFixedSize(235, 235)
+layout = QVBoxLayout()
+
+Button = QPushButton('GeNeRaTe MeMe')
+Button.clicked.connect(generate_meme)
+layout.addWidget(Button)
+window.setLayout(layout)
+window.show()
+sys.exit(meme_app.exec_())
